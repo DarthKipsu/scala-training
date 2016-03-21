f58d82df3c3b457d2deaa20b95c705f9becbc144
Verna Koskinen <darth.kipsu@gmail.com>
2016-03-21 21:23:16 +0200

    add excersise 1 b, print a list of leap years

--- ex1/s1.scala
+++ ex1/s1.scala
@@ -11,5 +11,35 @@ def triangle() = {
   }
 }
 
+def leap() = {
+
+  def isLeapYear(year: Int) = {
+    if (year % 100 == 0) (year % 400 == 0 && year != 4000)
+    else year % 4 == 0
+  }
+  
+  def iterDirection(year1: Int, year2: Int): Int = {
+    if (year1 < year2) 1
+    else -1
+  }
+
+  println("Give years: (separated by enter)")
+  val year1, year2 = readInt
+
+  var comma = false
+  for (year <- year1 to year2 by iterDirection(year1, year2)) {
+    if (isLeapYear(year)) {
+      if (comma) print(", ")
+      else comma = true
+      print(year)
+    }
+  }
+}
+
 println("a) Print a triangle by a given height.")
 triangle()
+
+println("\nb) Print leap years between (year year).")
+leap()
+
+println()
