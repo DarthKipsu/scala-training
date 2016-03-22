9f1e009b62117f2073afddf09ddd8b149088841c
Verna Koskinen <darth.kipsu@gmail.com>
2016-03-22 09:40:50 +0200

    add exercise 2 b, wind lines with given word from file

--- ex1/s2.scala
+++ ex1/s2.scala
@@ -1,3 +1,7 @@
+import java.io.FileNotFoundException
+
+import scala.annotation.tailrec
+import scala.io.Source
 import scala.io.StdIn._
 
 object pelaa extends App {
@@ -14,4 +18,37 @@ object pelaa extends App {
   println("Right answer was " + rightAnswer)
   if (winnings > 0) println("You win " + winnings + " virtual euros!")
   else println("You lose 100 virtual euros...")
+}
+
+object findWord extends App {
+  @tailrec
+  def findWordByUserInput(lines: List[String], word: String): Int = {
+    if (word == "") 0
+    else {
+      printLinesWith(lines, word, 1)
+      findWordByUserInput(lines, askForWord())
+    }
+  }
+
+  def askForWord(): String = {
+    println("\nMitä sanaa etsitään? Tyhjä lopettaa.")
+    readLine
+  }
+
+  @tailrec
+  def printLinesWith(lines: List[String], wordToSearch: String, line: Int): Int = {
+    if (lines.isEmpty) 0
+    else {
+      if (lines.head contains wordToSearch) println(line + ": " + lines.head)
+      printLinesWith(lines.tail, wordToSearch, line++)
+    }
+  }
+
+  try {
+    val lines = Source.fromFile(args(0)).getLines().toList
+    findWordByUserInput(lines, askForWord())
+  } catch {
+    case e: FileNotFoundException => println("Tiedostoa ei löytynyt!")
+    case e: ArrayIndexOutOfBoundsException => println("Anna tiedoston polku parametrina!")
+  }
 }
