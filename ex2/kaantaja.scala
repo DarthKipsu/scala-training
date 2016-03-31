c17613eb7a6a12095c8500f96eb2b12a32a73ba8
Verna Koskinen <darth.kipsu@gmail.com>
2016-03-31 09:11:16 +0300

    add error handling to dictionary

--- ex2/kaantaja.scala
+++ ex2/kaantaja.scala
@@ -1,5 +1,6 @@
-import scala.annotation.tailrec
+import java.io.FileNotFoundException
 
+import scala.annotation.tailrec
 import scala.io.Source
 import scala.io.StdIn._
 
@@ -18,9 +19,14 @@ object kaantaja extends App {
     }
   }
 
-  val lines = Source.fromFile(args(0)).getLines().grouped(2).toList
-  val dictionary = lines.map(s => s(0) -> s(1)).toMap
+  try {
+    val lines = Source.fromFile(args(0)).getLines().grouped(2).toList
+    val dictionary = lines.map(s => s(0) -> s(1)).toMap
 
-  println("Sanat opittu! (tyhjä kysymysrivi lopettaa)")
-  translate(dictionary)
+    println("Sanat opittu! (tyhjä kysymysrivi lopettaa)")
+    translate(dictionary)
+  } catch {
+    case e: FileNotFoundException => println("Sanakirjaa ei löytynyt!")
+    case e: ArrayIndexOutOfBoundsException => println("Anna sanakirjan polku parametrina!")
+  }
 }
