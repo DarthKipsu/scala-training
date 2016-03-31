5f46675e0e830077cda52478f0db44381fe5b265
Verna Koskinen <darth.kipsu@gmail.com>
2016-03-31 10:58:56 +0300

    add complex number calculation class

---
+++ ex2/complex.scala
@@ -0,0 +1,38 @@
+import scala.math.pow
+
+object complex_numbers extends App {
+  val a = Complex(5.16, 2.97)
+  val b = Complex(-3.7, -9.813)
+  println("a: " + a)
+  println("b: " + b)
+
+  println("\n-a: " + -a)
+  println("-b: " + -b)
+
+  println("\na+b: " + (a+b))
+  println("a-b: " + (a-b))
+  println("a*b: " + (a*b))
+  println("b^-1: " + b.inverse)
+  println("a/b: " + (a/b))
+}
+
+object Complex {
+  def apply(re: Double, im: Double) = {
+    new Complex(re, im)
+  }
+}
+
+class Complex(val re: Double, val im: Double) {
+
+  def +(x: Complex) = new Complex(re + x.re, im + x.im)
+  def unary_- = new Complex(-re, -im)
+  def -(x: Complex) = this + -x
+  def *(x: Complex) = new Complex(re * x.re - im * x.im, re * x.im + im * x.re)
+  def inverse = new Complex(re / (pow(re,2) + pow(im,2)), im / (pow(re,2) + pow(im,2)))
+  def /(x: Complex) = this * x.inverse
+
+  override def toString = {
+    if (im < 0) s"${re}${im}i"
+    else s"${re}+${im}i"
+  }
+}
