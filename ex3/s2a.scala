14427066996c5f7a85ceeb98420a93cd659e13ed
Verna Koskinen <darth.kipsu@gmail.com>
2016-04-13 15:00:21 +0300

    add prime number listing function

---
+++ ex3/s2a.scala
@@ -0,0 +1,23 @@
+
+object prime extends App {
+  println(Primes.first(15))       // First 15 primes
+  println(Primes.upTo(15))        // Primes until 15
+  println(Primes.between(15, 50)) // Primes between 15 and 50
+
+  object Primes {
+    val primeStream = Stream.from(2).filter(isPrime)
+
+    def isPrime(x: Int) = {
+      if (x < 2) false
+      else !(2 to (x - 1)).exists(x % _ == 0)
+    }
+
+    def first(n: Int) = primeStream.take(n).toList
+
+    def upTo(x: Int) = primeStream.takeWhile(_ <= x).toList
+
+    def between(x: Int, y: Int) = {
+      Stream.from(x).filter(isPrime).takeWhile(_ <= y).toList
+    }
+  }
+}
